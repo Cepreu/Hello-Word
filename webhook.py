2367f868632d5228dd3033c91561cd1750ed2379
Sergei Krupnov <skrupnov@gmail.com>
2018-08-22 17:42:08 -0700

    method->methods

--- webhook.py
+++ webhook.py
@@ -7,7 +7,7 @@ from flask import request
 from flask import make_response
 
 app = Flask(__name__)
-@app.route('/webhook', method=['POST'])
+@app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
     print(json.dumps(req, indent=4))
