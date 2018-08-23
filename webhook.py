dffc4825dc264c6ff6bae2f8cf42e004c6429506
Sergei Krupnov <skrupnov@gmail.com>
2018-08-22 17:47:31 -0700

    int(os...

--- webhook.py
+++ webhook.py
@@ -37,6 +37,6 @@ def makeResponse(req):
     }
 
 if __name__ == '__main__':
-    port = int.getenv('PORT', 5000)
+    port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
